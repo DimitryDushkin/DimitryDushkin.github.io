906d92815b34e22aa98ef4ef80b25c7c63cda952
Dmitrii Dushkin <dndushkin@fb.com>
2022-10-30 21:38:18 +0000

    remove some tests

--- test/test-generic-post.js
+++ test/test-generic-post.js
@@ -117,64 +117,7 @@ describe("check build output for a generic post", () => {
     });
 
     describe("body", () => {
-      it("should have images", () => {
-        const images = Array.from(
-          doc.querySelectorAll("article :not(aside) picture img")
-        );
-        const pictures = Array.from(
-          doc.querySelectorAll("article :not(aside) picture")
-        );
-        const metaImage = select("meta[property='og:image']", "content");
-        expect(images.length).to.greaterThan(0);
-        expect(pictures.length).to.greaterThan(0);
-        const img = images[0];
-        const picture = pictures[0];
-        const sources = Array.from(picture.querySelectorAll("source"));
-        expect(sources).to.have.length(3);
-        expect(img.src).to.match(/^\/img\/remote\/\w+-1920w\.jpg$/);
-        expect(metaImage).to.match(new RegExp(URL));
-        expect(metaImage).to.match(/\/img\/remote\/\w+\.jpg$/);
-        const avif = sources.shift();
-        const webp = sources.shift();
-        const jpg = sources.shift();
-        expect(jpg.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.jpg 1920w, \/img\/remote\/\w+-1280w.jpg 1280w, \/img\/remote\/\w+-640w.jpg 640w, \/img\/remote\/\w+-320w.jpg 320w/
-        );
-        expect(webp.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.webp 1920w, \/img\/remote\/\w+-1280w.webp 1280w, \/img\/remote\/\w+-640w.webp 640w, \/img\/remote\/\w+-320w.webp 320w/
-        );
-        expect(avif.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.avif 1920w, \/img\/remote\/\w+-1280w.avif 1280w, \/img\/remote\/\w+-640w.avif 640w, \/img\/remote\/\w+-320w.avif 320w/
-        );
-        expect(jpg.type).to.equal("image/jpeg");
-        expect(webp.type).to.equal("image/webp");
-        //expect(avif.type).to.equal("image/avif");
-        expect(jpg.sizes).to.equal("(max-width: 608px) 100vw, 608px");
-        expect(webp.sizes).to.equal("(max-width: 608px) 100vw, 608px");
-        expect(img.height).to.match(/^\d+$/);
-        expect(img.width).to.match(/^\d+$/);
-        expect(img.getAttribute("loading")).to.equal("lazy");
-        expect(img.getAttribute("decoding")).to.equal("async");
-        // JSDom fails to parse the style attribute properly
-        expect(img.outerHTML).to.match(/svg/);
-        expect(img.outerHTML).to.match(/filter/);
-      });
-
-      it("should have json-ld", () => {
-        const json = select("script[type='application/ld+json']");
-        const images = Array.from(
-          doc.querySelectorAll("article :not(aside) img")
-        );
-        const obj = JSON.parse(json);
-        expect(obj.url).to.equal(POST_URL);
-        expect(obj.description).to.equal(
-          "Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster..."
-        );
-        expect(obj.image.length).to.be.greaterThan(0);
-        obj.image.forEach((url, index) => {
-          expect(url).to.equal(URL + images[index].src);
-        });
-      });
+     
 
       it("should have paragraphs", () => {
         const images = Array.from(doc.querySelectorAll("article > p"));
